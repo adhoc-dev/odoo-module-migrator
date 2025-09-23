2c54ba10a3cf4364f7c96e58986ec2780ce2c803
Joaquin Arroyo <joa@adhoc.com.ar>
2025-09-23 11:10:19 -0300

    [IMP][ADD] Improvements in warnings logging, new progress bar and improvement in static code analysis

--- odoo_module_migrate/migration_scripts/migrate_180_190.py
+++
@@ -1,9 +0,0 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-# This script is based on the original code from:
-# https://github.com/odoo/odoo/blob/master/odoo/upgrade_code/17.5-00-tree-to-list.py
-
-from odoo_module_migrate.base_migration_script import BaseMigrationScript
-
-
-class MigrationScript(BaseMigrationScript):
-    _GLOBAL_FUNCTIONS = []
